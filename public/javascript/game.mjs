55fa952762737c0aa3e6de35163866f90927fe7a
SerGo3580 <SerGo3577@gmail.com>
2022-07-11 17:21:53 +0300

    implement client game room logic

--- public/javascript/game.mjs
+++ public/javascript/game.mjs
@@ -1,7 +1,24 @@
+import {roomSocketEventInit} from "./room.mjs";
+import {showMessageModal} from "./views/modal.mjs";
+
 const username = sessionStorage.getItem('username');
 
 if (!username) {
 	window.location.replace('/login');
 }
-
 const socket = io('', { query: { username } });
+
+const redirectToLogin = () => {
+	window.location.replace('/login');
+}
+
+socket.on('CONNECT_ERROR', () => {
+	sessionStorage.clear();
+	showMessageModal({
+		message: 'connect error this user already on server',
+		onClose: redirectToLogin
+	})
+});
+
+roomSocketEventInit(socket,username);
+
